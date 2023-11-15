1df03076ea2875678ded4a8b6a863063cfb6622d
wanderer <1379605319@qq.com>
2023-11-15 21:29:23 +0800

    initialization

---
+++ backstage/src/components/common/AppProvider.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ElConfigProvider } from 'element-plus';
+
+
+</script>
+<template>
+<el-config-provider>
+    <slot>
+        
+    </slot>
+</el-config-provider>
+</template>
